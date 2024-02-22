d1b603becea348238ba24993e57f07045f1512dc
Guilherme Girardi <guilherme@ardanis.com>
2024-02-22 11:13:13 -0300

    testing POST transaction

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,18 +1,31 @@
 import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+import { z } from 'zod'
 import { knex } from '../database'
 
+// Use "zod" to validate the request.body and make sure of what we are receiving from the FE.
+// If the FE is sending a right request.body.
 export async function transactionsRoutes(server: FastifyInstance) {
-  server.get('/agoravai', async () => {
-    const transactions = await knex('transactions')
-      // .insert({
-      //   id: randomUUID(),
-      //   title: 'Testing...',
-      //   amount: 9999,
-      // })
-      // .returning('*')
-      .where('amount', 999)
-      .select('*')
+  // reply = response
+  server.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
 
-    return transactions
+    // .parse() checks if the request.body has the correct properties.
+    // if not, .parse() will throw an new Error and stop everything below.
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body,
+    )
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    reply.status(201).send({ message: 'POST - WORKED!' })
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,10 @@ import { transactionsRoutes } from './routes/transactions'
 const server = fastify()
 
 // The plugins order is important!
-server.register(transactionsRoutes)
+server.register(transactionsRoutes, {
+  // prefix config
+  prefix: 'transactions',
+})
 
 server
   .listen({
