7847666ea11465242bbc2bbca4573b782bf2a846
Guilherme Girardi <guilherme@ardanis.com>
2024-02-23 13:34:07 -0300

    create get and get transaction route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,25 @@ import { knex } from '../database'
 // Use "zod" to validate the request.body and make sure of what we are receiving from the FE.
 // If the FE is sending a right request.body.
 export async function transactionsRoutes(server: FastifyInstance) {
+  server.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  server.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    // .first() = if I don't use it, "transaction" would be an array
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   // reply = response
   server.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
